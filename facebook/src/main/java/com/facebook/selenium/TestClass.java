3e9c3391e34f0e8305f5c26a0dde74ca74520f3f
Shaeq Khan <shaeqkhan@gmail.com>
2016-03-21 12:50:22 -0400

    checking conflicts...

--- facebook/src/main/java/com/facebook/selenium/TestClass.java
+++ facebook/src/main/java/com/facebook/selenium/TestClass.java
@@ -1,6 +1,7 @@
 public class TestClass {
 	//testing git commands
 	//adding another command in a branched stream
+	//a third comment that the other branch definitely doesn't have
 	
 	public static void main(String[] args) {}
 }
